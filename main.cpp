68d59cc66c1d4780b5af363e0c7a20c6ecf12ef0
Zhuowei(Alvin) Zhao <alvinzhaowei@gmail.com>
2025-08-25 13:43:02 +1000

    Update test case

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ struct Param parseArgs(int nargs, char **args) {
     char para[10];
     char graph_file[200] = "";
     char update_file[200] = "";
-    int w_type = 0;
+    int w_type = 1;
     float a = 0.5;
     float b = 0.5;
 //    printf("The input parameters are:\n\n");
@@ -141,88 +141,114 @@ int main(int argc, char **argv) {
     double delta = 0;
     double eps = 0;
     int L = (int) (log2(n));
-    //test delta from 1/100 to 1/1000
-    for (int i = 100; i <= 1000; i = i + 100) {
-        delta = 1.0 / i;
-        eps = 0.1 * 0.1 * delta / (L + 1) / 2;
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dis(1, (int) (L / delta));
-        int group_num = dis(gen);
-        std::vector<int> group_size(group_num);
-        std::vector<double> group_value(group_num);
-        int left = n;
-        for (int j = 0; j < group_num - 1; j++) {
-            std::uniform_int_distribution<int> dis2(1, left - (group_num - j - 1));
-            group_size[j] = dis2(gen);
-            left -= group_size[j];
-        }
-        group_size[group_num - 1] = left;
+    // test delta from 1/100 to 1/1000
+     for (int i = 100; i <= 1000; i = i + 100) {
+         delta = 1.0 / i;
+         eps = 0.1 * 0.1 * delta / (L + 1) / 2;
+         std::random_device rd;
+         std::mt19937 gen(rd());
+         std::uniform_int_distribution<int> dis(1, (int) (L / delta));
+         int group_num = dis(gen);
+         std::vector<int> group_size(group_num);
+         std::vector<double> group_value(group_num);
+         int left = n;
+         for (int j = 0; j < group_num - 1; j++) {
+             std::uniform_int_distribution<int> dis2(1, left - (group_num - j - 1));
+             group_size[j] = dis2(gen);
+             left -= group_size[j];
+         }
+         group_size[group_num - 1] = left;
 
-        double left_v = 1.0;
-        for (int j = 0; j < group_num; j++) {
-            std::uniform_real_distribution<double> realDist2(0.0, left_v);
-            group_value[j] = realDist2(gen);
-            left_v = left_v - group_value[j];
-            group_value[j] = group_value[j] / group_size[j];
-        }
+         double left_v = 1.0;
+         for (int j = 0; j < group_num; j++) {
+             std::uniform_real_distribution<double> realDist2(0.0, left_v);
+             group_value[j] = realDist2(gen);
+             left_v = left_v - group_value[j];
+             group_value[j] = group_value[j] / group_size[j];
+         }
 
-        start = getCurrentTime();
-        int z = 0;
-        double value = 0;
-        for (int j = 0; j < group_num; j++) {
-            for (int k = 0; k < group_size[j]; k++) {
-                x[z] = group_value[j];
-                z = z + 1;
-                value = value + group_value[j];
-            }
-        }
-        end = getCurrentTime();
-        total_time = end - start;
-        printf("Initialization time %.5f: %.9lf\t",
-               delta, total_time);
-        z = 0;
-        std::uniform_real_distribution<double> realDist(0.0, 1.0);
-        start = getCurrentTime();
-        for (int j = 0; j < group_num; ++j) {
-            double gv = group_value[j];
-            int gs = group_size[j];
+         start = getCurrentTime();
+         int z = 0;
+         double value = 0;
+         for (int j = 0; j < group_num; j++) {
+             for (int k = 0; k < group_size[j]; k++) {
+                 x[z] = group_value[j];
+                 z = z + 1;
+                 value = value + group_value[j];
+             }
+         }
+         end = getCurrentTime();
+         total_time = end - start;
+         printf("Initialization time %.5f: %.9lf\t",
+                delta, total_time);
+         z = 0;
+         std::uniform_real_distribution<double> realDist(0.0, 1.0);
+         start = getCurrentTime();
+         for (int j = 0; j < group_num; ++j) {
+             double gv = group_value[j];
+             int gs = group_size[j];
 
-            if (gv <= 0.0) continue;
+             if (gv <= 0.0) continue;
 
-            double p = gv / eps;
-            if (p >= 1.0) {
-                std::fill(r.begin() + z, r.begin() + z + gs, gv);
-                z += gs;
-            } else {
-                double log2_p_inv = 1.0 / std::log2(p);  // precompute
-                int o = 0;
-                while (o < gs) {
-                    double u = realDist(gen);
-                    int y = static_cast<int>(std::ceil(std::log2(u) * log2_p_inv));
-                    o += y;
-                    if (o >= gs) break;
-                    r[z + o] = eps;
-                }
-                z += gs;
-            }
-        }
+             double p = gv / eps;
+             if (p >= 1.0) {
+                 std::fill(r.begin() + z, r.begin() + z + gs, gv);
+                 z += gs;
+             } else {
+                 double log2_p_inv = 1.0 / std::log2(p);  // precompute
+                 int o = 0;
+                 while (o < gs) {
+                     double u = realDist(gen);
+                     int y = static_cast<int>(std::ceil(std::log2(u) * log2_p_inv));
+                     o += y;
+                     if (o >= gs) break;
+                     r[z + o] = eps;
+                 }
+                 z += gs;
+             }
+         }
 
-        end = getCurrentTime();
-        total_time = end - start;
-        printf("%.9lf\n",
-               total_time);
-    }
-    printf("---------------------------------------------------------------------\n");
+         end = getCurrentTime();
+         total_time = end - start;
+         printf("%.9lf\n",
+                total_time);
+     }
+     printf("---------------------------------------------------------------------\n");
 //query test
     printf("Query test:\n");
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> realDist(0.0, 1.0);
+    std::uniform_int_distribution<int> dist(0, n-1);
     float a = para.a;
     float b = para.b;
+    // float a = 0;
+    // float b = 1;
+
     auto *x_ = new double[n];
+    for (int i = 0; i < n; i++) {
+        x_[i] = 1.0/n;
+        // x_[i] = realDist(gen);
+    }
+
+    // normalize
+    // double sum = 0;
+    // for (int i = 0; i < n; i++) {
+    //     sum = sum + x_[i];
+    // }
+    // for (int i = 0; i < n; i++) {
+    //     x_[i] = 1.0 / sum;
+    // }
+
+    //one hot
+    // for (int i = 0; i < n; i++) {
+    //     x_[i] = 0;
+    // }
+    // x_[dist(gen)] = 1.0;
+
     L = (int) (log2(n));
+    delta = 1.0/n;
+    // L = L * 2;
     auto *w = new double[L + 1];
     if (para.w_type == 0) {
         for (int i = 0; i < L; i++) {
@@ -249,13 +275,11 @@ int main(int argc, char **argv) {
             w[i] = w[i - 1] * t / i;
         }
     }
-
+    // printf("L:%d\n", L);
     eps = 0.1 * 0.1 * delta / (L + 1) / 2;
+    // eps = 0.1 * 0.1 * delta / L / L;
+    // printf("eps:%.9f\n", eps);
     graph.query(x_, a, b, L, w, eps, 'S');
-
-
-
-
 
 
 //update test
@@ -280,7 +304,6 @@ int main(int argc, char **argv) {
     printf("---------------------------------------------------------------------\n");
     printf("total update time: %.9lf\t",
            total_update);
-
 
 }
 
